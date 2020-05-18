e52d7e90abfaad10edae9ef17b9498807522dce6
Hielito <fsedenoguerrero@uma.es>
2020-05-18 14:36:40 +0200

    Prototipo Interfaz

--- FlicksArchive/src/GUI/Principal.java
+++ FlicksArchive/src/GUI/Principal.java
@@ -1,14 +1,29 @@
 package GUI;
 
+import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame marco = new JFrame("Flicks Archive");
+		Rectangle rec = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+		
+		marco.setPreferredSize(new Dimension(rec.width, rec.height));
+		
+		
+		
 		VentanaPrincipal window = new VentanaPrincipal();
-		window.setVisible(true);
 		
+		marco.add(window);
 		
-		
+		marco.setResizable(true);
+		marco.pack();
+		marco.setVisible(true);
 		
 	}
 
--- FlicksArchive/src/GUI/VentanaPrincipal.java
+++ FlicksArchive/src/GUI/VentanaPrincipal.java
@@ -4,8 +4,19 @@ import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
 
 public class VentanaPrincipal extends JPanel {
+	public JTextField buscador;
 
 	/**
 	 * Create the panel.
@@ -13,9 +24,80 @@ public class VentanaPrincipal extends JPanel {
 	
 	
 	public VentanaPrincipal() {
+		setName("FlicksArchive");
 		Rectangle rec = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
 		setPreferredSize(new Dimension(rec.width-100, rec.height-100));
+		setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBorder(null);
+		tabbedPane.setPreferredSize(new Dimension(1920, 1020));
+		tabbedPane.setBounds(10, 11, 1800, 918);
+		add(tabbedPane);
+		
+		JPanel Lista = new JPanel();
+		tabbedPane.addTab("Lista", null, Lista, null);
+		Lista.setLayout(null);
+		
+		JButton flechader = new JButton("");
+		flechader.setBounds(1717, 11, 48, 48);
+		flechader.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/img/ra.png")));
+		Lista.add(flechader);
+		flechader.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JOptionPane.showMessageDialog(flechader, "Flecha Derecha");
+			}
+		});
+		
+		JButton flechaizq = new JButton("");
+		flechaizq.setBounds(1659, 11, 48, 48);
+		flechaizq.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JOptionPane.showMessageDialog(flechaizq, "Flecha Izquierda");
+			}
+		});
+		flechaizq.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/img/la.png")));
+		Lista.add(flechaizq);
+		
+		JPanel Añadir = new JPanel();
+		tabbedPane.addTab("A\u00F1adir", new ImageIcon(VentanaPrincipal.class.getResource("/img/anadir.png")), Añadir, null);
+		Añadir.setLayout(null);
+		
+		buscador = new JTextField();
+		buscador.setBounds(10, 32, 432, 23);
+		buscador.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				buscador.setText("");
+			}
+		});
+		Añadir.add(buscador);
+		buscador.setColumns(10);
+		
+		JLabel busca = new JLabel("Buscador:");
+		busca.setBounds(10, 11, 65, 14);
+		Añadir.add(busca);
+		
+		JButton aceptar = new JButton("Buscar");
+		aceptar.setBounds(447, 32, 89, 23);
+		aceptar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//JOptionPane.showMessageDialog(aceptar, "Resultado de la busqueda");
+				buscador.setText("");
+			}
+		});
+		Añadir.add(aceptar);
 
 	}
-
 }
