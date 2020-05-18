e4bc922c4b3db6fd40750ff612bf342f762e5014
Hielito <fsedenoguerrero@uma.es>
2020-05-18 10:51:19 +0200

    Principio GUI

---
+++ FlicksArchive/src/GUI/Principal.java
@@ -0,0 +1,15 @@
+package GUI;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VentanaPrincipal window = new VentanaPrincipal();
+		window.setVisible(true);
+		
+		
+		
+		
+	}
+
+}
---
+++ FlicksArchive/src/GUI/VentanaPrincipal.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+
+public class VentanaPrincipal extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public VentanaPrincipal() {
+		setPreferredSize(new Dimension(1000, 1000));
+
+	}
+
+}
