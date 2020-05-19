cf97601d448679246dafe8e232390de620946414
AlvaroVeraOrtiz <62576020+AlvaroVeraOrtiz@users.noreply.github.com>
2020-05-19 16:10:33 +0200

    Añadido main de prueba

---
+++ FlicksArchive/src/Main.java
@@ -0,0 +1,17 @@
+
+import java.sql.SQLException;
+
+import flicksArchive.Lista;
+
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+
+		
+		Lista prueba=new Lista("Alvaro");
+		System.out.println(prueba.toString());
+	}
+
+}
